22d1e2bb06099315c4f5229c23018478c81e531d
Thais Barbosa <thaismsbrasil@hotmail.com>
2021-12-01 11:04:11 -0300

    atualizacao

--- src/routes/doctors.js
+++ src/routes/doctors.js
@@ -1,8 +1,12 @@
-const express = require("express")
-const router = express.Router()
-const controller = require("../controllers/doctorController")
+const express = require("express");
+const router = express.Router();
+const controller = require("../controllers/doctorController");
 
-router.post("/doctors", controller.createDoctor)
-router.get("/doctors", controller.getAllDoctors)
+router.post("/doctors", controller.createDoctor);
+router.get("/doctors", controller.getAllDoctors);
+router.get("/:id", controller.getDoctor);
+router.put("/:id", controller.updateDoctor);
+router.patch("/:id/favorite", controller.updateFavorite);
+router.delete("/:id", controller.deleteDoctor);
 
 module.exports = router;
